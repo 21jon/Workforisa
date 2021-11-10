11c4d35b08913d6447c01ef6e7c0e85f58ab9aed
21Jon <l.jonas.1@web.de>
2021-11-10 09:31:43 +0100

    CLient Handler basic

---
+++ src/com/serv/ClientHandler.java
@@ -0,0 +1,136 @@
+package com.serv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class ClientHandler extends Thread {
+    private final Socket clientSocket;
+    private final Servermain server;
+    private String login = null;
+    private OutputStream outputStream;
+
+    public ClientHandler(Servermain server, Socket clientSocket) {
+        this.server = server;
+        this.clientSocket = clientSocket;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClientSocket() throws IOException, InterruptedException {
+        InputStream inputStream = clientSocket.getInputStream();
+        this.outputStream = clientSocket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] tokens = line.split(" ");
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("quit".equalsIgnoreCase(cmd) || "logoff".equalsIgnoreCase(cmd)) {
+                    handleLogoff();
+                    break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream, tokens);
+                } else {
+                    String msg = "unknown " + cmd + "\n";
+                    outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+                }
+            }
+        }
+
+        clientSocket.close();
+
+    }
+
+    private void handleLogoff() {
+        List<ClientHandler> workerList = server.getClients();
+        // send other online users current user's status
+        String onlineMsg = "offline " + login + "\n";
+        for (ClientHandler worker : workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) {
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if (login.equals("guest") && password.equals("guest") || login.equals("jim") && password.equals("jim")) {
+                String msg = "ok login\n";
+                try {
+                    outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                this.login = login;
+                System.out.println("User logged in succsesfully: " + login);
+
+                List<ClientHandler> workerList = server.getWorkerList();
+
+                // send current user all other online logins
+                for (ClientHandler worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online " + worker.getLogin() + "\n";
+                            send(msg2);
+                        }
+                    }
+                }
+                // send other online users current user's status
+                String onlineMsg = "online " + login + "\n";
+                for (ClientHandler worker : workerList) {
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+                    }
+                }
+            } else {
+                String msg = "error login\n";
+                try {
+                    outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void send(String msg) {
+        if (login != null) {
+            try {
+                outputStream.write((msg + "\r\n").getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/com/serv/Servermain.java
@@ -0,0 +1,46 @@
+package com.serv;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Servermain extends Thread {
+
+    ServerSocket serverSocket = null;
+    ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+
+    @Override
+    public void run() {
+        try {
+            serv();
+        } catch (Exception e) {
+            System.out.println("Ohoh, da ist ein Fehler, das ist nicht gut. :-<");
+        }
+    }
+
+    public void serv() throws IOException {
+        serverSocket = new ServerSocket();
+
+        while (true) {
+
+            System.out.println("Server is waiting for connection");
+            Socket s = serverSocket.accept();
+            ClientHandler handler = new ClientHandler(this, s);
+            handler.run();
+            System.out.println(s.getInetAddress() + " : Client socket accepted");
+
+        }
+
+    }
+
+    public ArrayList<ClientHandler> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<ClientHandler> clients) {
+        this.clients = clients;
+    }
+
+}
